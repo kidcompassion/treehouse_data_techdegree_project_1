7dbdb621690dc44fea8786ca8da678a9ab086985
Sally Poulsen <sallypoulsen@Sallys-MacBook-Pro.local>
2025-05-04 19:46:46 -0600

    Adds error handling.

--- guessing_game.py
+++ guessing_game.py
@@ -16,55 +16,65 @@ def start_game():
     # This will hold all guesses
     list_of_guesses = []
     # Welcome the user
-    print("|******** Welcome to the number guessing game! *********|\n")
+    print("\n|******** WELCOME TO THE NUMBER GUESSING GAME! *********|\n")
     
     # For fun, ask their name
-    username = str(input("Please enter your name: "))
+    username = str(input("Please enter your name: \n"))
     
     # Users can enter any name they want, but it has to be something.
     # While the username is empty, keep prompting them to put their name.
     while username == "":
-        username = str(input("You cannot leave this empty. Please enter your name: "))
+        username = str(input("You cannot leave this empty. Please enter your name: \n"))
     
     # Choose a random number
     generated_num = random.randint(1, 11)
 
     
-
+    
 
 
     #remove this later
     print(f"Number is {generated_num}")
     
-    # Get the user's guess
-    # add error handling
+    # Get the user's first guess
+    # Set a switch to ensure any errors get re-tried
+    first_guess = False
+    print(f"Hi {username}! I’ve just generated a secret number for you to guess.\n")
+    #While the switch is set to false, keep looping
+    while first_guess == False:
+        try:
+            #Let the user guess. If the guess is valid, set the switch to True to break out of the loop.
+            guess = int(input(f"What do you think the secret number is? \n"))
+            first_guess = True
+        except ValueError:
+            print("I'm sorry, that's not a valid guess! Please enter an integer between 1 and 10. \n")
+        
     
-    try:
-        guess = int(input(f"Hi {username}! I’ve just generated a number for you to guess. What do you think it is?"))
-    except ValueError:
-        print("This is a value error")
-    
-    
-    
-    #add the guess to the list
+    # once the guess is valid, add it to our list
     list_of_guesses.append(guess)
 
-    # until the guess matches the number, keep guessing
+    # until the guess matches the random number, keep guessing
     while guess != generated_num:
         if guess> generated_num:
             # if this is higher, alert the user
-            guess = int(input(f"Good guess, but that was too high. Guess again: "))
-            list_of_guesses.append(guess)
+            try:
+                guess = int(input(f"Good attempt, but that was too high. Guess again: \n"))
+                list_of_guesses.append(guess)
+            except ValueError:
+                print("Oops, invalid guess! Please enter an integer between 1 and 10. \n")
 
-        elif guess < generated_num:
+        else:
             #if this is lower
-            guess = int(input(f"So close! But that was too low. Guess again: "))
-            list_of_guesses.append(guess)
-        
+            try:
+                guess = int(input(f"So close! But that was too low. Guess again: \n"))
+                list_of_guesses.append(guess)
+            except ValueError:
+                print(f"Whoops! That was invalid - please enter an integer between 1 and 10. \n")
+
 
     print(list_of_guesses)
-    
-    print(f"You did it, {username}! Great job!")
+    print(f"You did it, {username}! Great job!\n")
+    print(f"|************* YOUR STATS **************|")
     print(f"You guessed {len(list_of_guesses)} times")
     print(f"Your median guess was {statistics.median(list_of_guesses)}")
     print(f"Your mean guess was {statistics.mean(list_of_guesses)}")
@@ -78,28 +88,28 @@ def start_game():
         print(f"Okay, {username}! Great work and we'll see you later.")
         sys.exit()
     
-# Write your code inside this function.
+    # Write your code inside this function.
 
-#   When the program starts, we want to:
-#   ------------------------------------
-#   1. Display an intro/welcome message to the player.
-#   2. Store a random number as the answer/solution.
-#   3. Continuously prompt the player for a guess.
-#     a. If the guess is greater than the solution, display to the player "It's lower".
-#     b. If the guess is less than the solution, display to the player "It's higher".
+    #   When the program starts, we want to:
+    #   ------------------------------------
+    #   1. Display an intro/welcome message to the player.
+    #   2. Store a random number as the answer/solution.
+    #   3. Continuously prompt the player for a guess.
+    #     a. If the guess is greater than the solution, display to the player "It's lower".
+    #     b. If the guess is less than the solution, display to the player "It's higher".
 
-#   4. Once the guess is correct, stop looping, inform the user they "Got it" and store the number of guesses it took in a list.
-#   5. Display the following data to the player
-#     a. How many attempts it took them to get the correct number in this game
-#     b. The mean of the saved attempts list
-#     c. The median of the saved attempts list
-#     d. The mode of the saved attempts list
-#   6. Prompt the player to play again
-#     a. If they decide to play again, start the game loop over.
-#     b. If they decide to quit, show them a goodbye message.
+    #   4. Once the guess is correct, stop looping, inform the user they "Got it" and store the number of guesses it took in a list.
+    #   5. Display the following data to the player
+    #     a. How many attempts it took them to get the correct number in this game
+    #     b. The mean of the saved attempts list
+    #     c. The median of the saved attempts list
+    #     d. The mode of the saved attempts list
+    #   6. Prompt the player to play again
+    #     a. If they decide to play again, start the game loop over.
+    #     b. If they decide to quit, show them a goodbye message.
 
-# ( You can add more features/enhancements if you'd like to. )
+    # ( You can add more features/enhancements if you'd like to. )
 
 
-# Kick off the program by calling the start_game function.
+    # Kick off the program by calling the start_game function.
 start_game()
